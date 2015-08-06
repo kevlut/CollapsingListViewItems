b93659d811165ae0a0a964ba80c1364de10f91c0
Kevin Luty <kevinluty@gmail.com>
2015-08-06 10:24:21 -0500

    Initial Commit

---
+++ CollapsingListViewItems/Car.cs
@@ -0,0 +1,61 @@
+﻿namespace CollapsingListViewItemContainers
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Simple model that implements INPC to raise property changed 
+    /// on IsVisible
+    /// </summary>
+    public class Car : INotifyPropertyChanged
+    {
+        private bool _isVisible;
+
+        public Car(string name, string title, int id)
+        {
+            Name = name;
+            Title = title;
+            Id = id;
+            IsVisible = true;
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public string Title
+        {
+            get; set;
+        }
+
+        public int Id
+        {
+            get; set;
+        }
+
+        public bool IsVisible
+        {
+            get
+            {
+                return _isVisible;
+            }
+            set
+            {
+                _isVisible = value;
+                NotifyPropertyChanged("IsVisible");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var handler = PropertyChanged;
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ CollapsingListViewItems/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace CollapsingListViewItemContainers
+{
+    using System.Collections.ObjectModel;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Set up collection of test data.
+        /// </summary>
+        private ObservableCollection<Car> _cars = new ObservableCollection<Car>
+                        {
+                            new Car("Not yours", "Mine", 1),
+                            new Car("Not mine", "Yours", 2),
+                            new Car("Not ours", "His", 3),
+                            new Car("Not ours", "Hers", 4),
+                        };
+
+        /// <summary>
+        /// Create view and set up binding (Cars)
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        /// <summary>
+        /// Collection of Cars (bound to view)
+        /// </summary>
+        public ObservableCollection<Car> Cars
+        {
+            get
+            {
+                return _cars;
+            }
+        }
+
+        /// <summary>
+        /// Quick visibility toggle switch for the Car in index 2.
+        /// </summary>
+        /// <param name="sender">Button raising event</param>
+        /// <param name="e">Button click arguments</param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Cars[2].IsVisible = !Cars[2].IsVisible;
+            DisButt.Content = Cars[2].IsVisible ? "Hide Car 3!" : "Show Car 3!";
+        }
+    }
+}
